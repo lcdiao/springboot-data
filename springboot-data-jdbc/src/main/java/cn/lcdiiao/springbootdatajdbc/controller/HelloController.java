dcdc19dce075a76028236a2343f135f67ad4720b
雕 <35444916+lcdiao@users.noreply.github.com>
2019-05-17 00:31:27 +0800

    springboot使用jdbc
    使用springboot的jdbc，以druid作为数据源，并配置好监控

---
+++ springboot-data-jdbc/src/main/java/cn/lcdiiao/springbootdatajdbc/controller/HelloController.java
@@ -0,0 +1,23 @@
+package cn.lcdiiao.springbootdatajdbc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/getHello")
+    @ResponseBody
+    public List map(){
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from user ");
+        return maps;
+    }
+}
