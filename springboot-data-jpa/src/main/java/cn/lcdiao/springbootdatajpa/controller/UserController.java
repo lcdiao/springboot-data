bacbc820dc0c58d10af09e557bbae5841a1e1a81
lcdiao <120879566@qq.com>
2019-05-21 18:04:37 +0800

    springboot使用jpa

---
+++ springboot-data-jpa/src/main/java/cn/lcdiao/springbootdatajpa/controller/UserController.java
@@ -0,0 +1,31 @@
+package cn.lcdiao.springbootdatajpa.controller;
+
+import cn.lcdiao.springbootdatajpa.entity.User;
+import cn.lcdiao.springbootdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id) {
+        Optional<User> byId = userRepository.findById(id);
+        User user = byId.get();
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user) {
+        User save = userRepository.save(user);
+        return save;
+    }
+}
